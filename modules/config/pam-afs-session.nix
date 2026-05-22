1f71189bc84cbbc2ff28341c37f6b15e1b7856b5
Anthony Wang <a@unnamed.website>
2026-05-22 16:22:08 -0400

    Enable pam-afs-session for sshd too

--- modules/config/pam-afs-session.nix
+++ modules/config/pam-afs-session.nix
@@ -13,17 +13,26 @@ in
 
   config = lib.mkIf config.nixathena.pam-afs-session.enable {
     # The Nix PAM config syntax is badly documented so see https://github.com/NixOS/nixpkgs/pull/255547
-    security.pam.services.login.rules = {
-      auth.afs = {
-        control = "optional";
-        modulePath = "${athena-pkgs.pam-afs-session}/lib/security/pam_afs_session.so";
-        order = 20000;
-      };
-      session.afs = {
-        control = "required";
-        modulePath = "${athena-pkgs.pam-afs-session}/lib/security/pam_afs_session.so";
-        order = 20000;
-      };
-    };
+    security.pam.services =
+      let
+        serviceCfg = service: {
+          rules = {
+            auth.afs = {
+              control = "optional";
+              modulePath = "${athena-pkgs.pam-afs-session}/lib/security/pam_afs_session.so";
+              order = 20000;
+            };
+            session.afs = {
+              control = "required";
+              modulePath = "${athena-pkgs.pam-afs-session}/lib/security/pam_afs_session.so";
+              order = 20000;
+            };
+          };
+        };
+      in
+      lib.flip lib.genAttrs serviceCfg [
+        "login"
+        "sshd"
+      ];
   };
 }
