ea9e689190ad2a8fd4598bc0d3e36e908e8a0dca
Anthony Wang <a@unnamed.website>
2026-05-24 01:53:25 -0400

    Enable add command by default

--- modules/default.nix
+++ modules/default.nix
@@ -55,18 +55,8 @@ in
         enable = lib.mkDefault true;
         cellName = lib.mkDefault "athena.mit.edu";
       };
-    }
-    (lib.mkIf cfg.workstation {
-      # TODO: Move some of this stuff into ./config
-      # Athena env vars
-      # This is what the dialups use
-      environment.variables = {
-        ATHENA_SYS = "amd64_ubuntu2204";
-        ATHENA_SYS_COMPAT = "i386_ubuntu2204:amd64_ubuntu2004:i386_ubuntu2004:amd64_deb100:i386_deb100:amd64_ubuntu1804:i386_ubuntu1804:amd64_deb90:i386_deb90:amd64_ubuntu1604:i386_ubuntu1604:amd64_deb80:i386_deb80:amd64_ubuntu1404:i386_ubuntu1404:amd64_deb70:i386_deb70:amd64_ubuntu1204:i386_ubuntu1204:amd64_ubuntu1004:i386_ubuntu1004:amd64_ubuntu904:i386_ubuntu904:amd64_deb50:i386_deb50:amd64_ubuntu804:i386_ubuntu804:amd64_deb40:i386_deb40:i386_rhel4";
-        HOSTTYPE = "linux";
-      };
-      programs = {
-        # `add` command
+      # `add` command
+      programs= {
         bash.interactiveShellInit = ''
           add ()
           {
@@ -78,9 +68,19 @@ in
             eval (attach -Padd -b $add_flags $argv)
           end
         '';
-        # For running dynamically linked stufff
-        nix-ld.enable = true;
       };
+    }
+    (lib.mkIf cfg.workstation {
+      # TODO: Move some of this stuff into ./config
+      # Athena env vars
+      # This is what the dialups use
+      environment.variables = {
+        ATHENA_SYS = "amd64_ubuntu2204";
+        ATHENA_SYS_COMPAT = "i386_ubuntu2204:amd64_ubuntu2004:i386_ubuntu2004:amd64_deb100:i386_deb100:amd64_ubuntu1804:i386_ubuntu1804:amd64_deb90:i386_deb90:amd64_ubuntu1604:i386_ubuntu1604:amd64_deb80:i386_deb80:amd64_ubuntu1404:i386_ubuntu1404:amd64_deb70:i386_deb70:amd64_ubuntu1204:i386_ubuntu1204:amd64_ubuntu1004:i386_ubuntu1004:amd64_ubuntu904:i386_ubuntu904:amd64_deb50:i386_deb50:amd64_ubuntu804:i386_ubuntu804:amd64_deb40:i386_deb40:i386_rhel4";
+        HOSTTYPE = "linux";
+      };
+      # For running dynamically linked stufff
+      programs.nix-ld.enable = true;
       # This creates /bin/* for better compatibility with Athena stuff
       services.envfs = {
         enable = true;
